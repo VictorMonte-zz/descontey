a3d3ebae98580275c7c9ae2b5e64a2910ec02bd2
Victor Monte <victorbcm@hotmail.com>
2019-11-01 23:44:36 -0300

    Bind grpc service with discount service

--- discount/src/app.ts
+++ discount/src/app.ts
@@ -1,9 +1,10 @@
 import DataBase from './config/db';
 import { of } from 'rxjs';
 import { serverBuilder } from 'rxjs-grpc';
-
 import { discount } from './grpc-namespaces';
-import { Server } from 'mongodb';
+import DiscountService from './modules/discount/DiscountService';
+import Discount from './modules/discount/Discount';
+import UserRepository from './modules/user/UserRepository';
 
 type ServerBuilder = discount.ServerBuilder;
 
@@ -14,12 +15,31 @@ class App {
 
   constructor() {
     this.database = new DataBase();
+  }
+
+  start() {
+
     this.dataBaseConnection();
 
     this.server = serverBuilder<ServerBuilder>('src/discount.proto', 'discount');
-    this.routes();
+
+    this.server.addDiscountService({
+      get(request) {
+
+        const discountService = new DiscountService();
+        const discount: Discount = discountService.get(request.userId, request.productId);
+
+        return of({
+          porcent: discount.porcent,
+          valueInCents: discount.valuesInCents
+        });
+      },
+    });
+
     this.server.start('0.0.0.0:50051');
-}
+
+    this.seed();
+  }
   
   dataBaseConnection() {
     this.database.createConnection();
@@ -30,15 +50,25 @@ class App {
   }
 
   routes() {
-    this.server.addDiscountService({
-      get(request) {
-        return of({
-          porcent: "1",
-          valueInCents: "2"
-        });
-      },
-    });
+    
+  }
+
+  seed() {
+
+    console.log("Starting seeding...");
+
+    // Remove test user
+    UserRepository
+      .deleteMany({id: "1"})
+      .then(user => console.log("Seed user removed"))
+      .catch(err => console.error(err));
+
+    // Add test user
+    UserRepository
+      .create({id: '1', firstName: 'Victor', lastName: 'Monte', dateOfBirth: new Date()})
+      .then(user => console.log("Seed user created"))
+      .catch(err => console.error(err));
   }
 }
 
-export default new App();
+export default App;
