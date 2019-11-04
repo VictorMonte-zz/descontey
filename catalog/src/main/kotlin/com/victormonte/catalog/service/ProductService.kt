7f5e062127d0ad7bb7ef1f8a40d2822f1fbc1ea9
Victor Monte <victorbcm@hotmail.com>
2019-11-04 09:28:59 -0300

    Alter get product service

--- catalog/src/main/kotlin/com/victormonte/catalog/service/ProductService.kt
+++ catalog/src/main/kotlin/com/victormonte/catalog/service/ProductService.kt
@@ -2,13 +2,51 @@ package com.victormonte.catalog.service
 
 import com.victormonte.catalog.domain.Product
 import com.victormonte.catalog.infrastructure.repository.ProductRepository
+import discount.Discount
+import discount.DiscountServiceGrpc
+import io.grpc.ManagedChannelBuilder
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 
 @Service
-class ProductService(val productRepository: ProductRepository) {
+class ProductService(private val productRepository: ProductRepository,
+                     private val discountService: DiscountService) {
 
     fun get(userId: String) : Flux<Product> {
-        return productRepository.findAll()
+
+        return productRepository
+                .findAll()
+                .map {
+                    var discount = discountService.get(userId, it.id)
+                    Product(
+                            it._id,
+                            it.id,
+                            it.price_in_cents,
+                            it.title,
+                            it.description,
+                            com.victormonte.catalog.domain.Discount(discount!!.porcent, discount.valueInCents))
+                }
+
+    }
+}
+
+@Service
+class DiscountService {
+
+    fun get(userId: String, productId: String): Discount.GetDiscountReply? {
+
+        val channel = ManagedChannelBuilder.forAddress("127.0.0.1", 50051)
+                .usePlaintext()
+                .build()
+
+        val discountServiceGrpc = DiscountServiceGrpc.newBlockingStub(channel)
+
+        val request = Discount.GetDiscountRequest
+                .newBuilder()
+                .setUserId(userId)
+                .setProductId(productId)
+                .build()
+
+        return discountServiceGrpc.get(request)
     }
 }
