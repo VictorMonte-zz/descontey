5156e4237cf8502d012f99b4e35842a11bfa65ba
Victor Monte <victorbcm@hotmail.com>
2019-11-09 12:30:33 -0300

    Remove files

--- discount-old/src/application/service/IDiscountService.ts
+++
@@ -1,8 +0,0 @@
-import GetDiscountCommand from "../command/GetDiscountCommand";
-import DiscountResponse from "../response/DiscountResponse";
-
-interface IDiscountService {
-    get(command: GetDiscountCommand): Promise<DiscountResponse>;
-}
-
-export default IDiscountService;
--- discount-old/src/infrastructure/config/Types.ts
+++
@@ -1,6 +0,0 @@
-export const TYPES = {
-    App: Symbol("App"),
-    Database: Symbol("Database"),
-    DiscountService: Symbol("DiscountService"),
-    DiscountServiceGrpc: Symbol("DiscountServiceGrpc")
-  };
