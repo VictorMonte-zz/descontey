0c6a9285107c51a786f390ccc807e3b70b386d25
Victor Monte <victorbcm@hotmail.com>
2019-11-16 17:31:13 -0300

    Test getting multiple products for a user

--- catalog/src/test/kotlin/service/ProductServiceTest.kt
+++ catalog/src/test/kotlin/service/ProductServiceTest.kt
@@ -8,10 +8,12 @@ import com.victormonte.catalog.service.ProductService
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import reactor.core.publisher.Flux
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ProductServiceTest {
 
@@ -74,5 +76,39 @@ class ProductServiceTest {
 
         assertEquals(expectedDiscount.valueInCents, result.discount.value_in_cents)
 
+    }
+
+    @Test
+    fun should_ReturnThreeProducts_When_GetProduct_For_User() {
+
+        val expectedProductCount = 3
+
+        val products = getFakeProducts(expectedProductCount)
+        val discount = discount.Discount.GetDiscountReply.newBuilder().setPorcent(5f).setValueInCents(1250).build()
+        Mockito.`when`(productRepository.findAll()).thenReturn(Flux.fromIterable(products))
+        Mockito.`when`(discountService.get(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(discount)
+
+        val result = productService.get("1")
+
+        assertEquals(expectedProductCount.toString(), result.count().block().toString())
+
+    }
+
+    private fun getFakeProducts(expectedProductCount: Int): ArrayList<Product> {
+        val products = ArrayList<Product>()
+        val discountDefault = Discount(pct = 0f, value_in_cents = 0)
+        for (id in 1..expectedProductCount) {
+
+            val product = Product(
+                    _id = UUID.randomUUID().toString(),
+                    id = id.toString(),
+                    priceInCents = id * 250,
+                    description = "Description" + id.toString(),
+                    title = "Game" + id.toString(),
+                    discount = discountDefault)
+
+            products.add(product)
+        }
+        return products
     }
 }
